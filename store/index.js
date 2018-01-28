24cfcd512f1dcebadaffa869b9faa1b9037585c4
unknown <adrian.wieprzkowicz@gmail.com>
2018-01-28 13:49:41 +0100

    Fix stores import

--- store/index.js
+++ store/index.js
@@ -1,7 +1,7 @@
 import Vuex from 'vuex'
 
-import i18n from '~/store/modules/common'
-import common from '~/store/modules/i18n'
+import i18n from '~/store/modules/i18n'
+import common from '~/store/modules/common'
 
 const createStore = () => {
 	return new Vuex.Store({
