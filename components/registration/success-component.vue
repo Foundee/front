6c30ad9551da391d6fa59efa525f37bcc5658feb
unknown <adrian.wieprzkowicz@gmail.com>
2018-01-29 21:54:33 +0100

    Add registration success view

---
+++ components/registration/success-component.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div>
+  	Welcome { get username from store }
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- pages/registration.vue
+++ pages/registration.vue
@@ -3,25 +3,29 @@
 		<h1 class="site-title">
 			{{ $t('registration.REGISTER_NEW_USER') }}
 		</h1>
-		<div class="">
 
+		<!-- if user isn't registered -->
+		<div v-if="!isRegistered">
 			<!-- info from back-end -->
 			<h2>Dołącz do Społeczności!</h2>
 			<p>To forum to miejsce, gdzie każdy może popracować zarówno nad rzeczywistymi umiejętnościami jak i sposobem wypowiadania się. Pracuj. Codziennie. Choć trochę. Ale regularnie. Takie podejście przełoży się na Twój sukces zawodowy. Nawiązuj ciekawe kontakty zawodowe i ćwicz umiejętności interpersonalne. Chodzi też o moment dyskusji, świeże spojrzenie, twórczą iskrę i interakcję z drugim człowiekiem, który podąża zupełnie innymi ścieżkami rozumowania niż Ty.</p>
-
 			<p>Rejestrując się potwierdzasz, że akceptujesz nasz Regulamin i zapoznałeś się z FAQ. Jest to ważne, abyśmy razem mogli zadbać o porządek i dobre zasady na forum.</p>
 			<!-- info from back-end -->
-
 			<hr>
-
 			<form-component @submit="onSubmit"></form-component>
+		</div>
 
+		<!-- if the user has registered -->
+		<div v-else>
+			<success-component></success-component>
 		</div>
 	</div>
 </template>
 
 <script>
 import formComponent from '@/components/registration/form-component.vue'
+import successComponent from '@/components/registration/success-component.vue'
+
 export default {
 	head () {
 		return {
@@ -29,11 +33,18 @@ export default {
 		}
 	},
 	components: {
-		formComponent
+		formComponent,
+		successComponent
+	},
+	data () {
+		return {
+			isRegistered: false
+		}
 	},
 	methods: {
-		onSubmit (data) {
-			console.log(data)
+		onSubmit (user) {
+			console.log(user)
+			this.isRegistered = true
 		}
 	}
 }
